550049c94a207a8fd4a9d5fa7352677f2c3aa405
Windows10 <Windows10@DESKTOP-4BLCQKE>
2022-10-12 14:00:13 +0700

    Update

--- batch13springboot/src/main/java/com/app/batch13springboot/model/MahasiswaModel.java
+++ batch13springboot/src/main/java/com/app/batch13springboot/model/MahasiswaModel.java
@@ -17,7 +17,7 @@ public class MahasiswaModel {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
-	private String age;
+	private int age;
 	private int semester;
 	
 
