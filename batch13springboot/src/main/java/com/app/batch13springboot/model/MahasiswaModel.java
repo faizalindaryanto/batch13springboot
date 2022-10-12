9a9ee734ebcd7e0adacc062fe45b4c1793dee62b
Windows10 <Windows10@DESKTOP-4BLCQKE>
2022-10-12 14:19:25 +0700

    Update

--- batch13springboot/src/main/java/com/app/batch13springboot/model/MahasiswaModel.java
+++ batch13springboot/src/main/java/com/app/batch13springboot/model/MahasiswaModel.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class MahasiswaModel {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private int age;
